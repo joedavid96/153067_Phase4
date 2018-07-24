cddc0cc598d2d64081431bf7742babf7e29739db
joedavid96 <joedavid96@gmail.com>
2018-07-24 18:41:13 +0530

    create WalletAlreadyExistsException.java

---
+++ src/com/cg/mywalletapp/exception/WalletAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.cg.mywalletapp.exception;
+
+@SuppressWarnings("serial")
+public class WalletAlreadyExistsException extends Exception {
+
+	public WalletAlreadyExistsException(String arg0) {
+		super(arg0);
+		
+	}
+ }
