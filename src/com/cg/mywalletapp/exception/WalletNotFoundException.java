6521ecdf0af67fbfb0396b53966cad961581a6a2
joedavid96 <joedavid96@gmail.com>
2018-07-24 18:43:15 +0530

    exceptions added

---
+++ src/com/cg/mywalletapp/exception/WalletNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cg.mywalletapp.exception;
+
+@SuppressWarnings("serial")
+public class WalletNotFoundException extends Exception {
+
+	public WalletNotFoundException(String arg0) {
+		super(arg0);
+	}
+}
---
+++ src/com/cg/mywalletapp/exception/WalletServicesDownException.java
@@ -0,0 +1,10 @@
+package com.cg.mywalletapp.exception;
+
+@SuppressWarnings("serial")
+public class WalletServicesDownException extends Exception {
+
+	public WalletServicesDownException(String arg0) {
+		super(arg0);
+		
+	}
+}
