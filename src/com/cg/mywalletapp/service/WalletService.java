8b06e7dfbf857694ef706aa4dd766b2c58f4c74c
joedavid96 <joedavid96@gmail.com>
2018-07-24 18:38:25 +0530

    update WalletService.java

--- src/com/cg/mywalletapp/service/WalletService.java
+++ src/com/cg/mywalletapp/service/WalletService.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import com.cg.mywalletapp.beans.Customer;
 import com.cg.mywalletapp.exception.InsufficientBalanceException;
-import com.cg.mywalletapp.exception.InvalidInputException;
+import com.cg.mywalletapp.exception.WalletAlreadyExistsException;
+import com.cg.mywalletapp.exception.WalletNotFoundException;
+import com.cg.mywalletapp.exception.WalletServicesDownException;
 
 public interface WalletService {
-public Customer createAccount(Customer customer);
-public Customer showBalance (String mobileno);
-public Customer fundTransfer (String sourceMobileNo,String targetMobileNo, BigDecimal amount) throws InvalidInputException, InsufficientBalanceException;
-public Customer depositAmount (String mobileNo,BigDecimal amount )throws InvalidInputException;
-public Customer withdrawAmount(String mobileNo, BigDecimal amount)throws InvalidInputException, InsufficientBalanceException;
-public List<String> getTransaction(String mob);
+public Customer createAccount(Customer customer) throws WalletServicesDownException, WalletAlreadyExistsException;
+public Customer showBalance (String mobileno) throws WalletNotFoundException;
+public Customer fundTransfer (String sourceMobileNo,String targetMobileNo, BigDecimal amount) throws WalletNotFoundException, InsufficientBalanceException, WalletServicesDownException;
+public Customer depositAmount (String mobileNo,BigDecimal amount )throws WalletNotFoundException, WalletServicesDownException;
+public Customer withdrawAmount(String mobileNo, BigDecimal amount)throws WalletNotFoundException, InsufficientBalanceException, WalletServicesDownException;
+public List<String> getTransaction(String mob) throws WalletServicesDownException;
+public List<Customer> getAllCustomers() throws WalletServicesDownException;
+public List<Customer> getCustomersWhoseBalanceLessThan() throws WalletServicesDownException;
 }
